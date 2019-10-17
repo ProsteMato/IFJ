83e1b46e14f9eb5a0df69cb7cb64d235c8540f5b
MisoKov <48593880+MisoKov@users.noreply.github.com>
2019-10-17 22:55:57 +0200

    scanner.c: start stav

--- scanner.c
+++ scanner.c
@@ -8,52 +8,90 @@
 
 #include "scanner.h"
 
-int get_next_token(FILE* source){
+
+int get_next_token(FILE *source, Token *token){
+	// kontrola source filu
+	if (source == NULL){
+		return 1;
+	} else if (token == NULL){
+		return 1;
+	}
+
 	// inicializacia premennych
 	char* str = malloc(DEFAULT_STR_LEN);
 	unsigned long str_i = 0;
 	char c;
-	int state = SCANST_START;
+	int state = START;
+	token->type = TK_EMPTY;
 
 	while(TRUE){
 		c = getc(source);
 		switch(state){
-			case (SCANST_START):
-				if (c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z' || c == '_'){
-					state = SCANST_START_OF_KW_ID;
-				} else if (c >= '0' && C <= '9'){
-					state = SCANST_NUMBER;
+			case (START):
+				if (c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z'){
+					state = ID_OR_KW;
+				} else if (isdigit(c)){
+					state = NUM;
 				} else if (isspace(c)){
-					stare = SCANST_START;
-				} else if (){
-					
-				} else if (){
-					
-				} else if (){
-					
-				} else if (){
-					
-				} else if (){
-					
-				} else if (){
-					
-				} else if (){
-					
-				} else if (){
-					
+					stare = START;
+				} else if (c == '_'){
+					state = ID;					
+				} else if (c == '#'){
+					state = LINE_COMMENT;					
+				} else if (c == '"'){
+					state = BLOCK_COMMENT0;						
+				} else if (c == 39){ // ascii 39 == '
+					state = BLOCK_COMMENT0;
+				} else if (c == '/'){
+					state = DIV;										
+				} else if (c == '='){
+					state = ASSIGN;
+				} else if (c == '<'){
+					state = LESSER;
+				} else if (c == '>'){
+					state = GREATER;
+				} else if (c == '!'){
+					state = NEG;
+				} else if (c == '\n'){
+					state = EOL;
+				} else if (c == '*'){
+					token->type = MULT;
+					free(str);
+					return 0;
+				} else if (c == '+'){
+					token->type = PLUS;
+					free(str);
+					return 0;
+				} else if (c == '-'){
+					token->type = MINUS;
+					free(str);
+					return 0;
+				} else if (c == ':'){
+					token->type = COLON;
+					free(str);
+					return 0;
+				} else if (c == '('){
+					token->type = BRACKET_L;
+					free(str);
+					return 0;
+				} else if (c == ')'){
+					token->type = BRACKET_R;
+					free(str);
+					return 0;
+				} else if (c == ','){
+					token->type = COMMA;
+					free(str);
+					return 0;
 				} else {
-
+					// lex chyba
+					return ;
 				}
 				break;
-			case (SCANST_EOL):
+			case (EOL):
 				break;
-			case (SCANST_START_OF_KW_ID):
+			case (ID_OR_KW):
 				break;
-			case (SCANST_NUMBER):
-				break;
-			case ():
-				break;
-			case ():
+			case (NUM):
 				break;
 			default:
 				break;
