107f820427dbc0975f3abe1c916e9c41f4a392c4
MisoKov <48593880+MisoKov@users.noreply.github.com>
2019-10-19 12:51:44 +0200

    scanner.c: dalsie stavy

--- scanner.c
+++ scanner.c
@@ -8,6 +8,45 @@
 
 #include "scanner.h"
 
+Keywords is_keyword(char* s, unsigned len){
+	if (strncmp (s, "def", len) == 0){
+		return KW_DEF;
+	} else if (strncmp (s, "else", len) == 0){
+		return KW_ELSE;
+	} else if (strncmp (s, "if", len) == 0){
+		return KW_IF;
+	} else if (strncmp (s, "None", len) == 0){
+		return KW_NONE;
+	} else if (strncmp (s, "pass", len) == 0){
+		return KW_PASS;
+	} else if (strncmp (s, "return", len) == 0){
+		return KW_RETURN;
+	} else if (strncmp (s, "while", len) == 0){
+		return KW_WHILE;
+	} else if (strncmp (s, "inputs", len) == 0){
+		return KW_INPUTS;
+	} else if (strncmp (s, "inputf", len) == 0){
+		return KW_INPUTF;
+	} else if (strncmp (s, "inputi", len) == 0){
+		return KW_INPUTI;
+	} else if (strncmp (s, "len", len) == 0){
+		return KW_LEN;
+	} else if (strncmp (s, "substr", len) == 0){
+		return KW_SUBSTR;
+	} else if (strncmp (s, "ord", len) == 0){
+		return KW_ORD;
+	} else if (strncmp (s, "chr", len) == 0){
+		return KW_CHR;
+	} else if (strncmp (s, "int", len) == 0){
+		return KW_INT;
+	} else if (strncmp (s, "str", len) == 0){
+		return KW_STR;
+	} else if (strncmp (s, "float", len) == 0){
+		return KW_FLOAT;
+	} else {
+		return -1;
+	}
+}
 
 int get_next_token(FILE *source, Token *token){
 	// kontrola source filu
@@ -22,6 +61,7 @@ int get_next_token(FILE *source, Token *token){
 	unsigned long str_i = 0;
 	char c;
 	int state = START;
+	int keyword = 0;
 	token->type = TK_EMPTY;
 
 	while(TRUE){
@@ -29,15 +69,21 @@ int get_next_token(FILE *source, Token *token){
 		switch(state){
 			case (START):
 				if (c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z'){
+					str[str_i] = c;
+					str_i++;
 					state = ID_OR_KW;
 				} else if (isdigit(c)){
+					str[str_i] = c;
+					str_i++;
 					state = NUM;
 				} else if (isspace(c)){
 					stare = START;
 				} else if (c == '_'){
+					str[str_i] = c;
+					str_i++;
 					state = ID;					
 				} else if (c == '#'){
-					state = LINE_COMMENT;					
+					state = LINE_COMMENT;				
 				} else if (c == '"'){
 					state = BLOCK_COMMENT0;						
 				} else if (c == 39){ // ascii 39 == '
@@ -84,14 +130,62 @@ int get_next_token(FILE *source, Token *token){
 					return 0;
 				} else {
 					// lex chyba
-					return ;
+					free(str);
+					return 1;
 				}
 				break;
-			case (EOL):
+			case (ID):
+				if (c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z' || isdigit(c) || c == '_'){
+					str[str_i] = c;
+					str_i++;
+					state = ID;
+				} else {
+					token->type = TK_ID;
+					token->atribute.string = str;
+					ungetc(c, source);
+					return 0;
+				}
 				break;
 			case (ID_OR_KW):
+				if (c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z'){
+					str[str_i] = c;
+					str_i++;
+					keyword = is_keyword(str, str_i);
+					state = ID_OR_KW;
+				} else if (isdigit(c) || c == '_'){
+					str[str_i] = c;
+					str_i++;
+					state = ID;
+				} else {
+					if (keyword >= 0){
+						token->type = TK_KW;
+						token->atribute.keyword = keyword;
+						ungetc(c, source);
+						free(str);
+						return 0;
+					} else {
+						token->type = TK_ID;
+						toke->atribute.string = str;
+						ungetc(c, source);
+						return 0;
+					}
+				}
 				break;
 			case (NUM):
+				if (isdigit(c){
+					str[str_i] = c;
+					str_i++;
+					state = NUM;
+				} else if (isdigit(c) || c == '_'){
+					str[str_i] = c;
+					state = ID;
+				} else {
+					token->type = TK_INT;
+					token->atribute.integer = strol(str,NULL,10);
+					ungetc(c,source);
+					free(str);
+					return 0;
+				}
 				break;
 			default:
 				break;
