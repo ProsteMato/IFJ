896e74b6a3b032d466770c63ec108ad41b4e3bd4
MisoKov <48593880+MisoKov@users.noreply.github.com>
2019-11-10 20:32:46 +0100

    error.h: definitions of error codes

---
+++ error.h
@@ -0,0 +1,26 @@
+/**
+ * Project: Implementace překladače imperativního jazyka IFJ19
+ *
+ * @brief Error codes definitions
+ *
+ * @author Michal Koval <xkoval17@stud.fit.vutbr.cz>
+ */
+#ifndef _ERROR_H
+#define _ERRORR_H
+
+// main program
+#define LEX_ERROR 1
+#define SYNTAX_ERROR 2
+#define SEM_FUNCTION_ERROR 3
+#define SEM_TYPE_ERROR 4
+#define SEM_PARAMS_ERROR 5
+#define OTHER_ERROR 6
+#define DIVISION_BY_ZERO_ERROR 9
+#define INTERNAL_ERROR 99
+
+// lex - scannner
+#define LEX_SUCCESS 10
+#define LEX_INTERNAL 11
+#define LEX_ERROR 12
+
+#endif //_ERROR_H
