04715d9b0124dd3b3f5d62d919254d52f28a388c
MisoKov <48593880+MisoKov@users.noreply.github.com>
2019-11-12 00:42:19 +0100

    my_string.h: dynamic string interface

---
+++ my_string.h
@@ -0,0 +1,35 @@
+/**
+ * Project: Implementace překladače imperativního jazyka IFJ19
+ *
+ * @brief String interface
+ *
+ * @author Michal Koval <xkoval17@stud.fit.vutbr.cz>
+ */
+
+#ifndef _MSTRING_H
+#define _MSTRING_H
+
+#include <stdlib.h>
+
+#define DEFAULT_STR_LEN 100
+
+/**
+ * @brief      Appends a character.
+ *
+ * @param      str   Pointer to the string
+ * @param      i     Pointer to the current index in string
+ * @param      cap   Pointer to the capability
+ * @param[in]  c     Character wich we want to append
+ *
+ * @return     0 in case of error, 1 in case of success
+ */
+int append_char (char *str, unsigned long *i, unsigned long *cap, char c);
+
+/**
+ * @brief      Initializes my string
+ *
+ * @return     Pointer to the inicialized string, NULL in case of error
+ */
+char* mystring_init();
+
+#endif //_MSTRING_H
