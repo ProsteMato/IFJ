45b9817530d3e6fe52565e2a1767bdad51c4a906
MaggieMima <48593629+MaggieMima@users.noreply.github.com>
2019-11-12 22:43:48 +0100

    Header file for expression parser
    Without the functions, for now

---
+++ expression_parser.h
@@ -0,0 +1,22 @@
+/**
+ *  Project: Implementace prekladača imperatívneho jazýka IFJ19
+ * 
+ * @brief Expression parser
+ * 
+ * @author Magdaléna Ondrušková <xondru16@stud.fit.vutbr.cz>
+ * 
+ */
+
+
+#ifndef _EXPRESSION_PARSER_H
+#define _EXPRESSION_PARSER_H
+
+#include "scanner.h"
+#include "error.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+#define tableSize 18 
+
+
+#endif //_EXPRESSION_PARSER_H
