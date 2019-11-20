a83879a18bd6db57d0f9416625a5a91a22b4c9ed
Zuzana98 <56517254+Zuzana98@users.noreply.github.com>
2019-11-20 16:35:46 +0100

    uprava funkcí tabulka symbolů

--- symtable.c
+++ symtable.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <error.h>
 
 #include "symtable.h"
 
@@ -12,32 +13,89 @@ void SymTabInitInit (tBSTNodePtr *RootPtr) {
 	*RootPtr = NULL;
 }
 
-//vyhledani promenne podle jejiho id
-int SymTabSearch (tBSTNodePtr RootPtr, char K, tData *Obsah)	{
+//vyhledani promenne podle jejiho id -- melo by byt hotove
+int SymTabSearch (tBSTNodePtr RootPtr, char K, tData **Obsah)	{
 
 	if (RootPtr == NULL) return FALSE; //neuspesne hledani
 	else if (K < RootPtr->Key)
-		return BSTSearch(RootPtr->LPtr, K, Content); //rekurzivni volani, hledam v levem podstromu
+		return BSTSearch(RootPtr->LPtr, K, *Obsah); //rekurzivni volani, hledam v levem podstromu
 	else if (K > RootPtr->Key)
-		return BSTSearch(RootPtr->RPtr, K, Content); //rekurzivni volani, hledam v pravem podstromu
+		return BSTSearch(RootPtr->RPtr, K, *Obsah); //rekurzivni volani, hledam v pravem podstromu
 	else{ //K=RootPtr->Key, uspesne hledani
-		*Obsah = RootPtr->BSTNodeCont->Data; //vracim data o promenne
+		*Obsah = RootPtr->Data; //vracim data o promenne
 		return TRUE;
 	}
 }
 
-//vlozeni noveho symbolu do tabulky
-void SymTabInsert (tBSTNodePtr* RootPtr, char K, tData Obsah){
+//vlozeni noveho symbolu do tabulky, musi bytint kvuli vraceni chyby
+int SymTabInsert (tBSTNodePtr* RootPtr, char K, tData *Obsah){
 	if (*RootPtr == NULL){ //pokud nebylo alokovane, alokuju
-		if ((*RootPtr = malloc (sizeof(struct tBSTNode))) == NULL) return; //zkontruluji jestli nedošlo k chybe
+		if ((*RootPtr = malloc (sizeof(struct tBSTNode))) == NULL) return INTERNAL_ERROR; //vratim chybu
 		(*RootPtr)->Key = K;
 		(*RootPtr)->Data = Obsah;
 		(*RootPtr)->LPtr = NULL;
-		(*RootPtr)->RPtr = NULL; //přidám položku
+		(*RootPtr)->RPtr = NULL;
 	}
 	else if (K < (*RootPtr)->Key)
 	    	BSTInsert (&(*RootPtr)->LPtr, K, Obsah);
 	else if (K > (*RootPtr)->Key)
 	    	BSTInsert (&(*RootPtr)->RPtr, K, Obsah);
 	else (*RootPtr)->Data = Obsah; //strom toto id uz obsahuje -> prepisu obsah
+}
+
+//ulozeni parametru do seznamu
+
+
+//pomocna funkce pro nahrazeni
+void ReplaceByRightmost (tBSTNodePtr PtrReplaced, tBSTNodePtr *RootPtr) {
+	if (*RootPtr == NULL || PtrReplaced == NULL) return;
+	if ((*RootPtr)->RPtr != NULL) ReplaceByRightmost(PtrReplaced, &(*RootPtr)->RPtr);
+	else{
+		PtrReplaced->Key = (*RootPtr)->Key;
+		PtrReplaced->Data = (*RootPtr)->Data;
+		tBSTNodePtr docasny  = (*RootPtr);
+		(*RootPtr) = (*RootPtr)->LPtr;
+		free(docasny);
+	}
+}
+
+//odstraneni symbolu s klicem k
+void SymTabDelete (tBSTNodePtr *RootPtr, char K) {
+	if (*RootPtr == NULL) return;
+
+	if ((*RootPtr)->Key < K) BSTDelete((&(*RootPtr)->RPtr), K);
+	else if ((*RootPtr)->Key > K) BSTDelete((&(*RootPtr)->LPtr), K);
+
+	else {
+		if ((*RootPtr)->LPtr == NULL && (*RootPtr)->RPtr == NULL){ //uzel nema podstrom
+			free (*RootPtr);
+			*RootPtr = NULL;
+		}
+		else if ((*RootPtr)->LPtr == NULL && (*RootPtr)->RPtr != NULL){
+			tBSTNodePtr docasny;
+			docasny = (*RootPtr)->RPtr;
+			free (*RootPtr);
+			*RootPtr = docasny;
+		}
+		else if ((*RootPtr)->LPtr != NULL && (*RootPtr)->RPtr == NULL){
+			tBSTNodePtr docasny;
+			docasny = (*RootPtr)->LPtr;
+			free (*RootPtr);
+			*RootPtr = docasny;
+		}
+		else {
+			if ((*RootPtr)->LPtr->RPtr != NULL) ReplaceByRightmost(*RootPtr, &(*RootPtr)->LPtr->RPtr);
+			else ReplaceByRightmost(*RootPtr, &(*RootPtr)->LPtr);
+		}
+	}
+}
+
+//zruseni stromu
+void SymTabDispose (tBSTNodePtr *RootPtr) {
+	if (*RootPtr != NULL) {
+		BSTDispose(&(*RootPtr)->LPtr);
+		BSTDispose(&(*RootPtr)->RPtr);
+		free (*RootPtr);
+		*RootPtr = NULL;
+	}
 }
