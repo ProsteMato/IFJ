c62a47411771d329e53e83c0c45b26826318f3ce
ProsteMato <xkocim05@stud.fit.vutbr.cz>
2019-11-27 18:28:24 +0100

    Implemented sem_analysis.

---
+++ sematic_analysis.c
@@ -0,0 +1,208 @@
+#include "sematic_analysis.h"
+
+
+bool is_build_in_function(char *function_id) {
+    if (strcmp(function_id, "inputs") == 0) return true;
+    if (strcmp(function_id, "inputi") == 0) return true;
+    if (strcmp(function_id, "inputf") == 0) return true;
+    if (strcmp(function_id, "print") == 0) return true;
+    if (strcmp(function_id, "len") == 0) return true;
+    if (strcmp(function_id, "substr") == 0) return true;
+    if (strcmp(function_id, "ord") == 0) return true;
+    if (strcmp(function_id, "chr") == 0) return true;
+    
+}
+
+void set_build_in_function_param_count(SymTabNodePtr root, char *function_id) {
+    if (strcmp(function_id, "inputs") == 0) SetParamCount(root, function_id, 0);
+    if (strcmp(function_id, "inputi") == 0) SetParamCount(root, function_id, 0);
+    if (strcmp(function_id, "inputf") == 0) SetParamCount(root, function_id, 0);
+    if (strcmp(function_id, "len") == 0) SetParamCount(root, function_id, 1);
+    if (strcmp(function_id, "substr") == 0) SetParamCount(root, function_id, 3);
+    if (strcmp(function_id, "ord") == 0) SetParamCount(root, function_id, 2);
+    if (strcmp(function_id, "chr") == 0) SetParamCount(root, function_id, 1);
+}
+
+int check_function_param_count(SymTabNodePtr root, int *function_id, int param_count) {
+    GlobalTableData global_data;
+    if (GlobalSymTabSearch(root, function_id, &global_data)) {
+        if (global_data.pocet_par == param_count) {
+            return OK;
+        } else {
+            return SEM_PARAMS_ERROR;
+        }
+    }
+    return SEM_FUNCTION_ERROR;
+}
+
+Data_type build_in_function_return_type(char *function_id) {
+    if (strcmp(function_id, "inputs") == 0) return TYPE_STRING;
+    if (strcmp(function_id, "inputi") == 0) return TYPE_INT;
+    if (strcmp(function_id, "inputf") == 0) return TYPE_FLOAT;
+    if (strcmp(function_id, "print") == 0) return TYPE_NONE;
+    // TODO odtialto nižšie sa kontroluje veľkosť vstupných parametrov a podľa toho sa určí navratová hodnota
+    // TODO toto dole asi bude treba zmazat neviem...
+    if (strcmp(function_id, "len") == 0) return TYPE_INT;
+    if (strcmp(function_id, "substr") == 0) return TYPE_STRING;
+    if (strcmp(function_id, "ord") == 0) return TYPE_STRING;
+    if (strcmp(function_id, "chr") == 0) return TYPE_STRING;
+    return TYPE_UNDEFINED;
+}
+
+int arithmetic_operation_check(Data_type first_type, pTable operation, Data_type second_type) {   
+    switch(operation) {
+        case PR_EDIVDIVE:
+            if(first_type == TYPE_INT || second_type == TYPE_INT) {
+                return OK;
+            } else {
+                return SEM_TYPE_ERROR;
+            }
+            break;
+        case PR_EPLUSE:
+            if(first_type == TYPE_STRING && first_type == TYPE_STRING) {
+                return OK;
+            }
+        case PR_EDIVE:
+        case PR_EMULTE:
+        case PR_EMINUSE:
+            if((first_type == TYPE_INT || first_type == TYPE_FLOAT) && (first_type == TYPE_INT || first_type == TYPE_FLOAT)) {
+                return OK;
+            }
+            return SEM_TYPE_ERROR;
+    }
+}
+
+int comparison_check(Data_type first_type, pTable relation, Data_type second_type) {
+    switch(relation) {
+        case PR_EEQE:
+        case PR_ENOTEQE:
+            return OK;
+        case PR_EGREATE:
+        case PR_EGREATEQE:
+        case PR_ELESSE:
+        case PR_ELESSEQE:
+            if (first_type == TYPE_STRING && second_type == TYPE_STRING) {
+                return OK;
+            } else if ((first_type == TYPE_INT || first_type == TYPE_FLOAT) && (second_type == TYPE_INT || second_type == TYPE_FLOAT)) {
+                return OK;
+            }
+        return SEM_TYPE_ERROR;
+    }
+}
+
+Data_type arithmetic_operation_return_type(Data_type first_type, Data_type second_type) {
+    if (first_type == TYPE_FLOAT && second_type == TYPE_FLOAT)
+        return TYPE_FLOAT;
+    if (first_type == TYPE_STRING && second_type == TYPE_STRING)
+        return TYPE_STRING;
+    if (first_type == TYPE_FLOAT && second_type == TYPE_INT)
+        return TYPE_FLOAT;
+    if (first_type == TYPE_INT && second_type == TYPE_FLOAT)
+        return TYPE_FLOAT;
+    if (first_type == TYPE_INT && second_type == TYPE_INT)
+        return TYPE_INT;
+    return TYPE_UNDEFINED;
+}
+
+Data_type get_type_from_token(SymTabNodePtr root, LocalTableNode *local_table, ParamList *params, Token token) {
+    
+    LocalTableData *local_data;
+    GlobalTableData *global_data;
+    switch(token.type) {
+        case TK_FLOAT:
+            return TYPE_FLOAT;
+        case TK_INT:
+            return TYPE_INT;
+        case TK_STRING:
+            return TYPE_STRING;
+        case TK_KW:
+            if(token.attribute == "None")
+                return TYPE_NONE;
+        default:
+            return TYPE_UNDEFINED;
+        case TK_ID:
+            if (LocalSymTabSearch(*local_table, token.attribute, &local_data))
+                return local_data->type;
+            if (GlobalSymTabSearch(root, token.attribute, &global_data))
+                return global_data->type;
+    }
+    return TYPE_UNDEFINED;
+}
+
+int is_function_defined(SymTabNodePtr root, char *function_id) {
+    GlobalTableData *global_data;
+    if (GlobalSymTabSearch(root, function_id, &global_data))
+        return true;
+    return false;
+}
+
+int is_global_variable(SymTabNodePtr root, char *id) {
+    GlobalTableData global_data;
+    if (GlobalSymTabSearch(root, id, &global_data)){
+        if (!global_data.funkce)
+            return true;
+    }
+    return false;
+}
+
+int is_variable_defined(SymTabNodePtr root, LocalTableNode *local_table, ParamList *params, char *variable_id) {
+    GlobalTableData global_data;
+    if (LocalSymTabSearchMinus(*local_table, variable_id))
+        return true;
+    if (ParamSearch(params, variable_id)) 
+        return true;
+    if (GlobalSymTabSearch(root, variable_id, &global_data)){
+        if (!global_data.funkce)
+            return true;
+    }
+    return false;
+}
+
+int define_function(SymTabNodePtr *root, char *function_id) {
+    if (is_function_defined(*root, function_id)) {
+        return SEM_FUNCTION_ERROR;
+    } else {
+        GlobalTableData data;
+        data.funkce = true;
+        GlobalSymTabInsert(root, function_id, &data);
+    }
+}
+
+char * expr_parser_gen_uniq_id(SymTabNodePtr root, LocalTableNode local_table) {
+    
+    static int id = 1;
+    char *name = "var";
+    char *uniq_id = NULL;
+    do {
+        char num = id + '0';
+        *uniq_id = (char *) malloc(sizeof(char) * strlen(name) + strlen(num) + 1);
+        if (uniq_id == NULL) {
+            return INTERNAL_ERROR;
+        }
+
+        strcpy(uniq_id, name);
+        strncat(uniq_id, num, strlen(num));
+        id++;
+    } while(!is_variable_defined(root, local_table, NULL, uniq_id));
+
+    return uniq_id;
+    
+}
+
+char * expr_parser_gen_uniq_id_with_prefix(SymTabNodePtr root, LocalTableNode local_table, char* prefix) {
+    static int id = 1;
+    char *uniq_id = NULL;
+    do {
+        char num = id + '0';
+        *uniq_id = (char *) malloc(sizeof(char) * strlen(num) + strlen(prefix) + 1);
+        if (uniq_id == NULL) {
+            return INTERNAL_ERROR;
+        }
+
+        strcpy(uniq_id, prefix);
+        strncat(uniq_id, num, strlen(num));
+        id++;
+    } while(!is_variable_defined(root, local_table, NULL, uniq_id));
+
+    return uniq_id;
+}
--- sematic_analysis.h
+++ sematic_analysis.h
@@ -13,6 +13,7 @@
 
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 #include "symtable.h"
 #include "scanner.h"
 #include "expression_parser.h"
@@ -29,17 +30,9 @@ bool is_build_in_function(char *function_id);
  * @brief This function will set build-in function param count
  * 
  * @param function_id Name/ID of the function
+ * @param root Pointer to global sym table
  */
-void set_build_in_function_param_count(char *function_id);
-
-/**
- * @brief This function will check if the build-in function have correct param count.
- * 
- * @param function_id Name/ID of the function
- * @param param_count Count of params
- * @return Error code SEM_PARAMS_ERROR
- */
-int check_build_in_function_param_count(char *function_id, int param_count);
+void set_build_in_function_param_count(SymTabNodePtr root, char *function_id);
 
 /**
  * @brief This function check if the given function have correct param count.
@@ -63,19 +56,21 @@ Data_type build_in_function_return_type(char *function_id);
  * @brief This function will check if two input types are compatible for arithmetic operation
  * 
  * @param first_type Type of first operand
+ * @param operation Type of operation
  * @param second_type Type of second operand
  * @return SEM_TYPE_ERROR
  */
-int arithmetic_operation_check(Data_type first_type, Data_type second_type);
+int arithmetic_operation_check(Data_type first_type, pTable operation, Data_type second_type);
 
 /**
  * @brief This function will check if two types are compatible for comparison operation
  * 
  * @param first_type Type of first operand
+ * @param relation Type of relation operator
  * @param second_type Type of second operand
  * @return SEM_TYPE_ERROR
  */
-int comparison_check(Data_type first_type, Data_type second_type);
+int comparison_check(Data_type first_type, pTable relation, Data_type second_type);
 
 /**
  * @brief Function will get return type from given 2 types
@@ -89,10 +84,13 @@ Data_type arithmetic_operation_return_type(Data_type first_type, Data_type secod
 /**
  * @brief Function will give type from token
  * 
+ * 
  * @param token_type token type
+ * @param root pointer to a global sym table
+ * @param local_table pointer to a actual local table.
  * @return data type
  */
-Data_type get_type_from_token(int token_type);
+Data_type get_type_from_token(SymTabNodePtr root, LocalTableNode *local_table, Token token);
 
 /**
  * @brief This function will check if function was defined.
@@ -104,16 +102,26 @@ Data_type get_type_from_token(int token_type);
 int is_function_defined(SymTabNodePtr root, char *function_id); // doplniť parametre
 
 /**
+ * @brief 
+ * 
+ * @param root This is pointer to a global sym table
+ * @param id Name/id of the global variable
+ * @return Bool if is global variable true otherwise false 
+ */
+int is_global_variable(SymTabNodePtr root, char *id);
+
+/**
  * @brief This function will check if the variable is defined if variable is not defined in local table of function or params
  *        it will find global table and its local table and try find there.
  * 
  * @param root This is pointer to a global sym table 
- * @param function_id If we are in definition of function this is Name/ID of this function otherwise NULL
+ * @param local_table If we are in definition of function this is local table of this function otherwise NULL
+ * @param params if is the function this will be pointer to a params of functiou otherwise NULL
  * @param variable_id Name/ID of variable
  * 
  * @return Error code SEM_FUNCTION_ERROR - if is not defined / OK - is defined
  */
-int is_variable_defined(SymTabNodePtr root, char *function_id, char *variable_id); // doplniť parametre
+int is_variable_defined(SymTabNodePtr root,  LocalTableNode *local_table, ParamList *params, char *variable_id); // doplniť parametre
 
 /**
  * @brief This function will define function in global sym table
@@ -128,19 +136,21 @@ int define_function(SymTabNodePtr *root, char *function_id);
 /**
  * @brief This function will generate uniq id for expr_parser
  * 
+ * @param root pointer to a global sym table
  * @param local_table if we are in function this is pointer to a local function otherwise pointer to a global sym table
  * @return uniq id
  */
-char * expr_parser_gen_uniq_id(SymTabNodePtr root);
+char * expr_parser_gen_uniq_id(SymTabNodePtr root, LocalTableNode local_table);
 
 /**
  * @brief This function will generate uniq id for expr_parser with prefix
  * 
  * @param root  Pointer to a global sym table.
+ * @param local_table Pointer to a local table of the function.
  * @param function_id If we are in definition of function this is Name/ID of this function otherwise NULL
  * @param prefix Prefix for uniq id.
  * @return uniq id
  */
-char * expr_parser_gen_uniq_id_with_prefix(SymTabNodePtr root, char *function_id, char* prefix);
+char * expr_parser_gen_uniq_id_with_prefix(SymTabNodePtr root, LocalTableNode local_table, char *function_id, char* prefix);
 
 #endif
