5f92ebdbae6772f99b7a27c4322e952ef026610f
ProsteMato <xkocim05@stud.fit.vutbr.cz>
2019-11-29 00:34:23 +0100

    updated tests for sem_test

--- sem_tests/sem_test.c
+++ sem_tests/sem_test.c
@@ -1,5 +1,6 @@
 #include "../sematic_analysis.h"
 #include <stdio.h>
+#include "sem_test.h"
 
 
 int main(int argc, char const *argv[])
@@ -7,7 +8,7 @@ int main(int argc, char const *argv[])
     char *build_in[] = {"inputi", "inputf", "inputs", "ord", "chr", "substr", "print", "len"};
     char *build_in_without_print[] = {"inputi", "inputf", "inputs", "ord", "chr", "substr", "len"};
     int id[] = {0, 0, 0, 2, 1, 3, 1};
-    SymTabNodePtr *root = malloc(sizeof(struct SymTabNode));
+    root = malloc(sizeof(struct SymTabNode));
     GlobalSymTabInit(root);
 
     printf("Kontrola build in functions\n");
@@ -51,6 +52,56 @@ int main(int argc, char const *argv[])
         }
     }
     printf("Kontrola parametrov build in functions == OK\n");
+    printf("kontrola aritmetických operacii\n");
+    if(arithmetic_operation_check(TYPE_FLOAT, PR_EPLUSE, TYPE_FLOAT) != OK){
+        printf("chyba 1\n");
+        return 0;
+    }
+    if(arithmetic_operation_check(TYPE_INT, PR_EPLUSE, TYPE_FLOAT) != OK){
+        printf("chyba 2\n");
+        return 0;
+        }
+    if(arithmetic_operation_check(TYPE_FLOAT, PR_EPLUSE, TYPE_INT) != OK){
+        printf("chyba 3\n");
+        return 0;
+        }
+    if(arithmetic_operation_check(TYPE_STRING, PR_EPLUSE, TYPE_STRING) != OK){
+        printf("chyba 4\n");
+        return 0;
+        }
+    if(arithmetic_operation_check(TYPE_STRING, PR_EPLUSE, TYPE_FLOAT) == OK){
+        printf("chyba 5\n");
+        return 0;
+        }
+    if(arithmetic_operation_check(TYPE_FLOAT, PR_EPLUSE, TYPE_STRING) != OK){
+        printf("chyba 6\n");
+        return 0;
+        }
+    if(arithmetic_operation_check(TYPE_INT, PR_EDIVDIVE, TYPE_INT) != OK){
+        printf("chyba 7\n");
+        return 0;
+        }
+    if(arithmetic_operation_check(TYPE_STRING, PR_EDIVDIVE, TYPE_FLOAT) == OK){
+        printf("chyba 8\n");
+        return 0;
+        }
+    if(arithmetic_operation_check(TYPE_FLOAT, PR_EDIVDIVE, TYPE_FLOAT) == OK){
+        printf("chyba 9\n");
+        return 0;
+        }
+    if(arithmetic_operation_check(TYPE_STRING, PR_EDIVE, TYPE_STRING) == OK){
+        printf("chyba 10\n");
+        return 0;
+        }
+    printf("kontrola aritmetických operacii == OK\n");
+    define_global_variable(root, "var1");
+    if(!is_variable_defined(*root, NULL, NULL, "var1")) {
+        printf("chyba premenna ma byt definovaná\n");
+        return 0;
+    }
+    printf("created global variable\n");
+    char *variable = expr_parser_gen_uniq_id(root, NULL);
+    printf("%s\n", variable);
     GlobalSymTabDispose(root);
     return 0;
 }
---
+++ sem_tests/sem_test.h
@@ -0,0 +1,6 @@
+
+#include "../symtable.h"
+#ifndef AAA
+#define AAA
+SymTabNodePtr *root;
+#endif
